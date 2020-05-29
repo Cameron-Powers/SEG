79c286cc16af29e9e052aff07b6c8ea492c0dc29
cameronpowers <cameronjpowers@gmail.com>
2020-05-29 14:28:41 -0400

    Changed navigation and design

---
+++ Lab3/scripts/main.js
@@ -0,0 +1,102 @@
+
+// This function is called when any of the tab is clicked
+// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function openInfo(evt, tabName) {
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+}
+
+
+	
+// generate a checkbox list from a list of products
+// it makes each product name as the label for the checkbos
+
+function populateListProductChoices(slct1, slct2) {
+    var s1 = document.getElementById(slct1);
+    var s2 = document.getElementById(slct2);
+	
+	
+    s2.innerHTML = "";
+		
+	
+    var optionArray = restrictListProducts(products, s1.value);
+
+		
+	for (i = 0; i < optionArray.length; i++) {
+			
+		var productName = optionArray[i];
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+		
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(productName));
+		s2.appendChild(label);
+		
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));    
+	}
+}
+	
+// This function is called when the "Add selected items to cart" button in clicked
+// The purpose is to build the HTML to be displayed (a Paragraph) 
+// We build a paragraph to contain the list of selected items, and the total price
+
+function selectedItems(){
+	
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+	var organic = document.getElementById('organic');
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+	
+	//Build a list of selected items
+	var para = document.createElement("P");
+	para.innerHTML = "You selected : ";
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) { 
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
+		
+	// Add the paragraph and price number at the bottom of the Page
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Total Price is $" + getTotalPrice(chosenProducts).toFixed(2)));
+}
+
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
+}
+
+
